00e6e9c6d92290e02332fe66450f84db5a908a66
Antoine <crbsantoine@gmail.com>
2020-07-12 15:02:13 +0200

    hold key instead of press once

--- scrypt.py
+++ scrypt.py
@@ -1,23 +1,17 @@
 import RPi.GPIO as GPIO
+from pynput import keyboard
 
 a = 26
-b = 19
-c = 13
-d = 6
+b = 13
+c = 6
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(a, GPIO.OUT)
 GPIO.setup(b, GPIO.OUT)
 GPIO.setup(c, GPIO.OUT)
-GPIO.setup(d, GPIO.OUT)
 
 def setNumber(n):
-    if (n//8==1):
-        n-=8
-        GPIO.output(d, GPIO.HIGH)
-    else: GPIO.output(d, GPIO.LOW)
-
     if (n//4==1):
         n-=4
         GPIO.output(c, GPIO.HIGH)
@@ -31,31 +25,37 @@ def setNumber(n):
     if (n==1): GPIO.output(a, GPIO.HIGH)
     else: GPIO.output(a, GPIO.LOW)
 
-def stop(): setNumber(0)
+def forwardFast(): setNumber(0)
 def forward(): setNumber(1)
 def backward(): setNumber(2)
 def rightSpin(): setNumber(3)
 def leftSpin(): setNumber(4)
+def stop(): setNumber(7)
 
-while True:
-    inkey = input("move ?")
-    if inkey == 'a': 
-        print('stop')
-        stop()
-    if inkey == 'z': 
+
+def on_press(key):
+    if key.char == 'z': 
         print('forward')
         forward()
-    if inkey == 's':
+    elif key.char == 's':             
         print('backward')
         backward()
-    if inkey == 'd': 
-        print('rspin')
+    elif key.char == 'd': 
+        print('rightSpin')
         rightSpin()
-    if inkey == 'q': 
-        print('lspin')
+    elif key.char == 'q': 
+        print('leftSpin')
         leftSpin()
-    if inkey == 'e': 
-        print('end')
-        break
+    elif key.char == 'a':
+        print('forwardFast')
+        forwardFast()
+
+def on_release(key):
+    stop()
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
 
 GPIO.cleanup()
