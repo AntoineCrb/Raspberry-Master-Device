933d9b6f7c1ba728ed74619986f886acb593cda6
Antoine <crbsantoine@gmail.com>
2020-07-18 17:00:24 +0200

    add fps system

--- autonomous.py
+++ autonomous.py
@@ -3,13 +3,21 @@ import numpy as np
 from camera.camera import Camera
 from communication.control import Control
 import algo
+import threading
+
+fps = 10
 
 camera = Camera()
 control = Control()
 
-while 1: # loop
+
+def loop():
     image = cv2.imread(camera.capture)
     lane_image = np.copy(image)
     camera.clear()
 
-    algo.canny(lane_image)
+    canny = algo.canny(lane_image)
+    cv2.imshow(canny)
+    threading.Timer(1/fps, loop).start()
+
+loop()
