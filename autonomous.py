02df3689b29b152dcfe7855d54c654bec519afc2
Antoine <crbsantoine@gmail.com>
2020-07-18 17:44:19 +0200

    update loop

--- autonomous.py
+++ autonomous.py
@@ -5,19 +5,26 @@ from communication.control import Control
 import algo
 import threading
 
-fps = 10
-
+fps = 0
 camera = Camera()
 control = Control()
 
+def init(fps=10):
+    fps = fps
+    #camera = Camera()
+    #control = Control()
+    loop()
 
 def loop():
-    image = cv2.imread(camera.capture)
-    lane_image = np.copy(image)
-    camera.clear()
+    for frame in camera.get_camera().capture_continuous(camera.get_raw_camera(), format="bgr", use_video_port=True):
+        image = np.copy(frame.array)
+        canny = algo.canny(image)
+        cv2.imshow(canny)
+        camera.get_raw_camera().truncate(0)
+        
+        #threading.Timer(1/fps, loop).start()
+    cv2.destroyAllWindows()
 
-    canny = algo.canny(lane_image)
-    cv2.imshow(canny)
-    threading.Timer(1/fps, loop).start()
 
-loop()
+
+init()
--- camera/camera.py
+++ camera/camera.py
@@ -5,14 +5,18 @@ class Camera:
     camera={}
     raw_capture={}
 
-    def __init__(self):
+    def __init__(self, fps=30):
         self.camera = PiCamera()
         self.camera.rotation = 180
         self.camera.resolution = (1280, 720)
+        self.camera.framerate = fps
         self.raw_capture = PiRGBArray(self.camera)
 
     def capture(self):
         self.camera.capture(self.raw_capture, format="bgr")
         return self.raw_capture.array
+
+    def get_camera(self): return self.camera
+    def get_raw_camera(self): return self.raw_capture
 
     def clear(self): self.raw_capture.truncate(0)
